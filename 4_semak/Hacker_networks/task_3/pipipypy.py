89755340e28668babc8df51868a75de896ef2bde
Sambuca72 <artemstolyarov91567@gamil.com>
2026-03-19 01:17:03 +0700

    new task3

--- 4_semak/Hacker_networks/task_3/pipipypy.py
+++ 4_semak/Hacker_networks/task_3/pipipypy.py
@@ -6,13 +6,12 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 try:
-    driver.get("https://quotes.toscrape.com/login")
-    driver.find_element(By.ID, "username").send_keys("Hacker777")
+    driver.get('https://quotes.toscrape.com/login')
+    driver.find_element(By.ID, "username").send_keys("Yana Czist")
     driver.find_element(By.ID, "password").send_keys("LevLox")
-    
     driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
     time.sleep(2)
-    
+
     with open("result.csv", "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(["Текст цитаты", "Автор", "Теги", "Ссылка на автора"])
@@ -24,21 +23,19 @@ try:
             for quote in quotes:
                 text = quote.find_element(By.CLASS_NAME, "text").text
                 author = quote.find_element(By.CLASS_NAME, "author").text
-                
-                tags_elements = quote.find_elements(By.CLASS_NAME, "tag")
-                tags = ", ".join([tag.text for tag in tags_elements])
-                
+                tags_elem = quote.find_elements(By.CLASS_NAME, "tag")
+                tags = ", ".join([tag.text for tag in tags_elem])
                 author_link = quote.find_element(By.TAG_NAME, "a").get_attribute("href")
-                
+
                 writer.writerow([text, author, tags, author_link])
                 writer.writerow([])
-                
+
             try:
-                next_button = driver.find_element(By.CSS_SELECTOR, "li.next > a")
-                next_button.click()
+                next_page = driver.find_element(By.CSS_SELECTOR, "li.next > a")
+                next_page.click()
                 time.sleep(2)
             except:
-                print("Парсинг окончен")
-                break       
+                print("Канец")
+                break
 finally:
     driver.quit()
